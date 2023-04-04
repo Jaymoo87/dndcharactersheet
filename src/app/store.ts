3716a853af46c8bf8df819093769527fa1f948f9
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-04-04 13:17:12 -0500

    reducers placed

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import attributeReducer from "../features/attributes/attributeSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    attributes: attributeReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
